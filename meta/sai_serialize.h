31e68237d87c7629516b372142aabf006a8e0cae
dhanasekar-arista <dhanasekar@arista.com>
2025-11-21 05:05:18 +0000

    refactored changes

--- meta/sai_serialize.h
+++ meta/sai_serialize.h
@@ -30,6 +30,8 @@ sai_status_t transfer_attributes(
 
 // serialize
 
+std::string sai_serialize_port_attr(_In_ const sai_port_attr_t port_attr);
+
 std::string sai_serialize_fdb_event(
         _In_ sai_fdb_event_t event);
 
@@ -450,6 +452,10 @@ void sai_deserialize_ipmc_entry_type(
         _In_ const std::string& s,
         _Out_ sai_ipmc_entry_type_t& type);
 
+void sai_deserialize_port_attr(
+      _In_ const std::string& s,
+      _Out_ sai_port_attr_t& port_attr);
+
 void sai_deserialize_l2mc_entry_type(
         _In_ const std::string& s,
         _Out_ sai_l2mc_entry_type_t& type);
@@ -769,3 +775,14 @@ void sai_deserialize_macsec_post_status_ntf(
         _In_ const std::string& s,
         _Out_ sai_object_id_t& macsec_id,
         _Out_ sai_macsec_post_status_t& macsec_post_status);
+
+
+// PORT_SNR_LIST serialization functions
+std::string sai_serialize_port_snr_list(
+        _In_ const sai_port_snr_list_t& snr_list,
+        _In_ bool countOnly);
+
+void sai_deserialize_port_snr_list(
+        _In_ const std::string& s,
+        _Out_ sai_port_snr_list_t& snr_list,
+        _In_ bool countOnly);
